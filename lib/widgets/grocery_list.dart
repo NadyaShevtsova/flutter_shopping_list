bbfd0b62be0dfa656743c56558dba158167ddc50
Nadia <nadia.shevtsova@oceanshq.com>
2023-11-24 14:20:36 +1000

    226. Error Response Handling

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -16,6 +16,7 @@ class GroceryList extends StatefulWidget {
 class _GroceryListState extends State<GroceryList> {
   List<GroceryItem> _groceryItems = [];
   var _isLoading = true;
+  String? _error;
 
   @override
   void initState() {
@@ -27,7 +28,14 @@ class _GroceryListState extends State<GroceryList> {
     final url = Uri.https(
         'flutter-prep1-4f071-default-rtdb.asia-southeast1.firebasedatabase.app',
         'shopping-list.json');
+
     final response = await http.get(url);
+
+    if(response.statusCode >= 400) {
+      setState(() {
+        _error = 'Failed to fetch data, please try later again.';
+      });
+    }
     final Map<String, dynamic> listData = json.decode(response.body);
     final List<GroceryItem> loadedItems = [];
     for (final item in listData.entries) {
@@ -100,6 +108,10 @@ class _GroceryListState extends State<GroceryList> {
           ),
         ),
       );
+    }
+
+    if (_error != null) {
+      content = Center(child: Text(_error!));
     }
 
     return Scaffold(
